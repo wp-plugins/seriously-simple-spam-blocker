c9e11eeda7b3548b88b612e603e83d6d01683951
voldemortensen <voldemortensen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-17 00:18:51 +0000

    tagging version 2.1
    git-svn-id: https://plugins.svn.wordpress.org/seriously-simple-spam-blocker/tags/2.1@1091922 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- seriously-simple-spam-blocker.php
+++ seriously-simple-spam-blocker.php
@@ -1,26 +1,22 @@
 <?php
 /**
  * Plugin Name: Seriously Simple Spam Blocker
- * Plugin URI: http://www.hughlashbrooke.com/portfolio-item/wordpress-plugin-seriously-simple-spam-blocker/
- * Description: A form validation plugin that replaces unfriendly CAPTCHA fields with a simple drag-and-drop image
- * Version: 1.0.4
- * Author: Hugh Lashbrooke
- * Author URI: http://www.hughlashbrooke.com/
- * Requires at least: 3.0
- * Tested up to: 3.4.2
+ * Plugin URI: http://jmortensen.bluehoststaff.com/?utm_source=wp_plugin_header&utm_medium=plugin&utm_campaign=sssb
+ * Description: A plugin that interacts with the StopForumSpam.com api to protect your website.
+ * Version: 2.1
+ * Author: Garth Mortensen
+ * Author URI: http://jmortensen.bluehoststaff.com/?utm_source=wp_plugin_header&utm_medium=plugin&utm_campaign=home
+ * Text Domain: sssb
+ * Tested up to: 4.1
  * 
  * @package Seriously Simple Spam Blocker
- * @author Hugh Lashbrooke
+ * @author Garth Mortensen
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-require_once( 'classes/class-seriously-simple-spam-blocker.php' );
+define( 'SSSB_BASE_DIR', plugin_dir_path( __FILE__ ) );
+define( 'SSSB_BASE_URL', plugin_dir_url( __FILE__ ) );
 
-global $ss_spamblocker;
-$ss_spamblocker = new SeriouslySimpleSpamBlocker( __FILE__ );
-
-if( is_admin() ) {
-	require_once( 'classes/class-seriously-simple-spam-blocker-admin.php' );
-	$ss_spamblocker_admin = new SeriouslySimpleSpamBlocker_Admin( __FILE__ );
-}
+require_once( SSSB_BASE_DIR . 'spam.php' );
+require_once( SSSB_BASE_DIR . 'admin/page.php' );
